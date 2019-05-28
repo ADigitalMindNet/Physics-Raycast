cb785ee42496466efb6b9d4ea9bf513006c77307
ADigitalMindNet <50277107+ADigitalMindNet@users.noreply.github.com>
2019-05-28 12:51:35 +0100

    Initial
    Initial

---
+++ Assets/CapsuleScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsuleScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		float Rotation = Input.GetAxis("Horizontal");
+		transform.Rotate(new Vector3(0, 0, -Rotation));
+
+		RaycastHit hit;
+
+		Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.up) * 10f, Color.red);
+
+		if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up), out hit, Mathf.Infinity))
+		{
+			hit.collider.gameObject.GetComponent<CubeScript>().BeenHit();
+			Debug.Log("I've hit something!");
+		}
+    }
+}
---
+++ Assets/CubeScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+	public Material hit;
+	public Material notHit;
+	private bool _isHit = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (_isHit)
+		{
+			GetComponent<Renderer>().material = hit;
+		} else
+		{
+			GetComponent<Renderer>().material = notHit;
+		}
+		_isHit = false;
+        
+    }
+
+	public void BeenHit()
+	{
+		_isHit = true;
+	}
+}
